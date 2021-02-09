8f30be69b3ce4f41cfb132daa977ffdc6c2df98c
Terry Brown <tabrownj@gmail.com>
2021-02-09 15:36:04 -0500

    Added followers list

--- src/App.js
+++ src/App.js
@@ -6,16 +6,26 @@ import './App.css';
 class App extends React.Component{
   state = {
     user:[],
+    followers: [],
   }
 
   componentDidMount(){
     axios.get('https://api.github.com/users/beardedwrench')
       .then(res => {
-        console.log(res.data)
         this.setState({
           user: res.data,
         })
       })
+      .catch( err => console.log( err ) )
+
+    axios.get('https://api.github.com/users/beardedwrench/followers')
+      .then( res => {
+        console.log(res.data)
+        this.setState({
+          followers: res.data,
+        })
+      })
+      .catch( err => console.log( err ) )
   }
 
   render(){
@@ -24,10 +34,25 @@ class App extends React.Component{
         <div className="card">
           <div className="avatar"><img src={ this.state.user.avatar_url } alt={ this.state.user.login } /></div>
           <div className="info">
-            <h2><a href={ this.state.user.html_url } target="_blank" ref="norefferer">{ this.state.user.login }</a></h2>
+            <h2><a href={ this.state.user.html_url } target="_blank">{ this.state.user.login }</a></h2>
             <h3>Followers: { this.state.user.followers }</h3>
           </div>
         </div>
+
+        <div className="card">
+          <div className="followers">
+            <h4>Followers</h4>
+
+            <ul>
+              {
+                this.state.followers.map( item => {
+                    return <li><img src={item.avatar_url}/><a href={item.html_url} target="_blank">{item.login}</a></li>
+                })
+              }
+            </ul>
+          </div>
+        </div>
+
       </div>
     )
   }
